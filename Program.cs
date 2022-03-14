9aa3b9714e058acb451ea065f499bdfdd79c6dc0
leeool <lgonsalez02@gmail.com>
2022-03-14 00:01:48 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string nome;
+string sobrenome;
+
+//pedir nomes
+Console.Write("Digite seu nome: ");
+nome = Console.ReadLine()!;
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+//nome completo
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("\nNome Completo: ");
+Console.ResetColor();
+Console.WriteLine($"{nome} {sobrenome}");
+
+//catálago
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Catálogo: ");
+Console.ResetColor();
+Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
